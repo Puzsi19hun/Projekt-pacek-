fa7116d3cb1fc229506f31ee560ac8d1e96533bb
Puzsi19hun <125877529+Puzsi19hun@users.noreply.github.com>
2023-06-06 23:07:35 +0200

    A kosárba tett elemek számolása
    Pacek

--- js/puzsi.js
+++ js/puzsi.js
@@ -1,57 +1,59 @@
 let products = [
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/1.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/2.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/3.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/4.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/5.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/6.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/7.png",
         price: 2000
     },
     {
         id: 1,
-        nev: "elso",
+        name: "elso",
         img: "images/product/8.png",
         price: 2000
     }
 ]
 
+let listCards = [];
+let list = document.querySelector(".list")
 
-
-
+let num = 0
+let numhere1 = 0
 let body = document.querySelector('body');
 
 let openasd = document.querySelector(".kocsi")
@@ -70,10 +72,106 @@ closeasd.addEventListener('click',  ()=>{
 }) 
 
 
+function initApp(key)
+{
+    products.forEach((value, key))
+    {
+        let newDiv = document.createElement("div")
+        newDiv.classList.add('item')
+        newDiv.innerHTML = `<img src="assets/imagies/${value.img}"
+        <div class="nev">${value.nev}</div>
+        <div class="ar">${value.price.toLocaleString()} ft</div>
+        <button onclick = "addToCard(${key})">Kosárba</button>}`;
+        list.appendChild(newDiv)
+    }
+}
+function addToCard(key)
+{
+    if(listCards[key] == null)
+    {
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+
+
 function kosarba(id)
 {
-    if(id == "1")
+    if(id == "egy")
     {
-        document.getElementById("ide").innerHTML += "Asked"
+
+        
+        num = Number(num) + Number(document.getElementById(1).value)
+        numhere1 = Number(numhere1) + Number(document.getElementById(1).value)
+        console.log(numhere1)
+
+        document.getElementById("db").innerHTML = num
+        document.getElementById("div").innerHTML += ""
+
     }
+    if(id == "ketto")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(2).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    if(id == "harom")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(3).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    if(id == "negy")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(4).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    if(id == "ot")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(5).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    if(id == "hat")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(6).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    if(id == "het")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(7).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    if(id == "nyolc")
+    {
+
+        
+        num = Number(num) + Number(document.getElementById(8).value)
+
+        
+        document.getElementById("db").innerHTML = num
+    }
+    
 }
